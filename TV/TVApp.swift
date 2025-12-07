ad2ddbb0ea0312a0a2f240596a85e9f56b6df609
ryalilakshman123 <lakshmanryali123@gmail.com>
2025-12-07 19:35:10 +0530

    Initial Commit

---
+++ TV/TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVApp.swift
+//  TV
+//
+//  Created by Sameer Nikhil on 07/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
